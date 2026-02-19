75e927c80bcd9dc65883fcb1bf4812fd26d1b96e
C.LAKSHANAA <c.lakshanaa@gmail.com>
2026-02-19 11:49:48 +0530

    Update y.py

--- y.py
+++ y.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 X = [[30],[39],[40],[50],[60],[20],[10],[70]]
-y = [0,,0,1,1,1,0,0,1]
+y = [0,0,1,1,1,0,0,1]
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X,y)
 X_marks=[[70]]
